6ab20a35ced4926fd78564bb6e32af25bd42e9b3
jiop <jeremie.iop@gmail.com>
2015-09-18 13:50:30 +1000

    Activate ":focus" rspec feature

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,8 @@ RSpec.configure do |config|
   end
 
   config.order = :random
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
